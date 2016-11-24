4adda57648f135543bbf88c36d438e1d8e600dc6
lukas <lukas.hofmaier@zuehlke.com>
2016-11-24 21:32:14 +0100

    create HttpClientActor
    The actor is responsible for sending messages to the competitionrunner server. We use an actor because the http requests are blocking.

---
+++ bot/bot-server/src/main/java/com/zuehlke/hoc/examplebot/HttpClientActor.java
@@ -0,0 +1,15 @@
+package com.zuehlke.hoc.examplebot;
+
+import akka.actor.UntypedActor;
+
+/**
+ * @author Lukas Hofmaier
+ */
+public class HttpClientActor extends UntypedActor {
+
+    @Override
+    public void onReceive(Object o) throws Throwable {
+
+    }
+}
+    
