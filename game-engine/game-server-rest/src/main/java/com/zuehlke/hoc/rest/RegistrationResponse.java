78274f1493b6cb8ca30734504b136d8895ec1022
lukas <lukas.hofmaier@zuehlke.com>
2016-11-25 13:07:11 +0100

    refined javadoc of RegistrationResponse.

--- game-engine/game-server-rest/src/main/java/com/zuehlke/hoc/rest/RegistrationResponse.java
+++ game-engine/game-server-rest/src/main/java/com/zuehlke/hoc/rest/RegistrationResponse.java
@@ -3,6 +3,8 @@ package com.zuehlke.hoc.rest;
 /**
  * Represents a message to confirm or reject a player registration. The competition runner responses with a
  * <code>RegistrationResponse</code> when it receives a <code>RegistrationMessage</code>.
+ * The <code>RegistrationResponse</code> tells the bot whether the registration was successul, or the registration
+ * phase is closed or the submitted player name or uri is already taken.
  *
  * @author Lukas Hofmaier
  */
