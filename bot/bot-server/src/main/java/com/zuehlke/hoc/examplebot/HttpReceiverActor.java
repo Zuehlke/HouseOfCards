825f9fcf54653f4967cce2d58bf052470ebbc35a
lukas <lukas.hofmaier@zuehlke.com>
2016-11-25 18:40:59 +0100

    Convert HTTP requests to RegistrationResponse object and forward them to the player actor.

--- bot/bot-server/src/main/java/com/zuehlke/hoc/examplebot/HttpReceiverActor.java
+++ bot/bot-server/src/main/java/com/zuehlke/hoc/examplebot/HttpReceiverActor.java
@@ -1,7 +1,11 @@
 package com.zuehlke.hoc.examplebot;
 
+import akka.actor.ActorRef;
 import akka.camel.CamelMessage;
 import akka.camel.javaapi.UntypedConsumerActor;
+import com.zuehlke.hoc.rest.RegistrationResponse;
+import org.apache.camel.converter.stream.InputStreamCache;
+import org.codehaus.jackson.map.ObjectMapper;
 
 /**
  * Listens for HTTP requests, transforms incoming requests to <code>RegistrationResponse</code> and forwards them to the
@@ -14,8 +18,12 @@ public class HttpReceiverActor extends UntypedConsumerActor {
     //the bot will reveive incomming HTTP request from the competition runner on this URI
     private String endpointUri;
 
-    public HttpReceiverActor(String listenOnUri) {
+    //reference to actor that reacts an game events sent by the competition server
+    private ActorRef playerActor;
+
+    public HttpReceiverActor(String listenOnUri, ActorRef playerActor) {
         this.endpointUri = String.format("jetty:%1", listenOnUri);
+        this.playerActor = playerActor;
     }
 
     @Override
@@ -28,6 +36,14 @@ public class HttpReceiverActor extends UntypedConsumerActor {
         //incomming HTTP request are sent as CamelMessage to the mailbox of this actor
         if (message instanceof CamelMessage) {
             CamelMessage camelMessage = (CamelMessage) message;
+
+            //retrieve json from HTTP Request and convert it into a RegistrationResponse
+            Object body = camelMessage.body();
+            InputStreamCache inputStreamCache = (InputStreamCache) body;
+            ObjectMapper objectMapper = new ObjectMapper();
+            RegistrationResponse registrationResponse = objectMapper.readValue(inputStreamCache, RegistrationResponse.class);
+
+            this.playerActor.tell(registrationResponse, getSelf());
 
             //return a message to the sender. This will return a HTTP response to the HTTP request sender
             // and close the TCP stream
