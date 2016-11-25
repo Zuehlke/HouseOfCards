c531db095429ff7e4b4b09187379974c93612f50
lukas <lukas.hofmaier@zuehlke.com>
2016-11-25 12:57:45 +0100

    create RegistrationResponse
    This class is used as a contract to exchange a confirmation (or rejection) from the competition runner to a bot.

---
+++ game-engine/game-server-rest/src/main/java/com/zuehlke/hoc/rest/RegistrationResponse.java
@@ -0,0 +1,10 @@
+package com.zuehlke.hoc.rest;
+
+/**
+ * Represents a message to confirm or reject a player registration. The competition runner responses with a
+ * <code>RegistrationResponse</code> when it receives a <code>RegistrationMessage</code>.
+ *
+ * @author Lukas Hofmaier
+ */
+public class RegistrationResponse {
+}
