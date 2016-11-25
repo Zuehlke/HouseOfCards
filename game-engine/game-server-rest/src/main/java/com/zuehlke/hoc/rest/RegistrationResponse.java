eb4ef5acce2435b93a26bf71209d5311d3f4fb75
lukas <lukas.hofmaier@zuehlke.com>
2016-11-25 13:17:06 +0100

    create enum to represent the result of the registration

--- game-engine/game-server-rest/src/main/java/com/zuehlke/hoc/rest/RegistrationResponse.java
+++ game-engine/game-server-rest/src/main/java/com/zuehlke/hoc/rest/RegistrationResponse.java
@@ -3,10 +3,14 @@ package com.zuehlke.hoc.rest;
 /**
  * Represents a message to confirm or reject a player registration. The competition runner responses with a
  * <code>RegistrationResponse</code> when it receives a <code>RegistrationMessage</code>.
- * The <code>RegistrationResponse</code> tells the bot whether the registration was successul, or the registration
+ * The <code>RegistrationResponse</code> tells the bot whether the registration was successful, or the registration
  * phase is closed or the submitted player name or uri is already taken.
  *
  * @author Lukas Hofmaier
  */
 public class RegistrationResponse {
+
+    private enum Result {
+        CONFIRMATION, NAME_ALREADY_TAKEN, URI_ALREADY_TAKEN, REGISTRATION_CLOSED
+    }
 }
