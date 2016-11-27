04934971816d744ded1979d0e03ce02b27facb9e
lukas <lukas.hofmaier@zuehlke.com>
2016-11-27 15:01:27 +0100

    configure the same receiving endpoint in the powershell testscript as in the runconfiguration

--- bot/bot-server/sendHttpPostRequestWithBody.ps1
+++ bot/bot-server/sendHttpPostRequestWithBody.ps1
@@ -1,5 +1,5 @@
 ﻿#In order to smoke test the example noker bot this script sends a HTTP post request with a JSON body to the define URI. 
-$url = "http://localhost:8875"
+$url = "http://localhost:8081"
 $body = "{`"registrationResult`": `"CONFIRMATION`"}"
 $contentType = "application/json"            
 
