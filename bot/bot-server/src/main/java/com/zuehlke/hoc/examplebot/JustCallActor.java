6ad22a61c81a19b97cfd3b009f92ee1d38d10e8a
lukas <lukas.hofmaier@zuehlke.com>
2016-11-27 14:31:45 +0100

    document JustCallActor

--- bot/bot-server/src/main/java/com/zuehlke/hoc/examplebot/JustCallActor.java
+++ bot/bot-server/src/main/java/com/zuehlke/hoc/examplebot/JustCallActor.java
@@ -3,6 +3,10 @@ package com.zuehlke.hoc.examplebot;
 import akka.actor.UntypedActor;
 
 /**
+ * Reacts on messages from the competition runner and decides the next move. This actor will register itself and
+ * afterward it will send the move "call" if it has enough credit. When the game is over it will terminated the
+ * application.
+ *
  * @author Lukas Hofmaier
  */
 public class JustCallActor extends UntypedActor {
