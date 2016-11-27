4b87330f217e8dae35da50a099839c4b70e51fbb
lukas <lukas.hofmaier@zuehlke.com>
2016-11-27 14:20:18 +0100

    create actor that decides the next move.

---
+++ bot/bot-server/src/main/java/com/zuehlke/hoc/examplebot/JustCallActor.java
@@ -0,0 +1,13 @@
+package com.zuehlke.hoc.examplebot;
+
+import akka.actor.UntypedActor;
+
+/**
+ * @author Lukas Hofmaier
+ */
+public class JustCallActor extends UntypedActor {
+    @Override
+    public void onReceive(Object o) throws Throwable {
+
+    }
+}
