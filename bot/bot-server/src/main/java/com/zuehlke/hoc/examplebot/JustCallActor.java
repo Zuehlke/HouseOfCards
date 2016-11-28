557c10691be8aca1943c51f7b0e8afd09a8d3885
lukas <lukas.hofmaier@zuehlke.com>
2016-11-28 13:54:55 +0100

    Receive and process incoming messages in JustCallActor.

--- bot/bot-server/src/main/java/com/zuehlke/hoc/examplebot/JustCallActor.java
+++ bot/bot-server/src/main/java/com/zuehlke/hoc/examplebot/JustCallActor.java
@@ -1,6 +1,9 @@
 package com.zuehlke.hoc.examplebot;
 
 import akka.actor.UntypedActor;
+import com.zuehlke.hoc.rest.GameEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Reacts on messages from the competition runner and decides the next move. This actor will register itself and
@@ -10,8 +13,17 @@ import akka.actor.UntypedActor;
  * @author Lukas Hofmaier
  */
 public class JustCallActor extends UntypedActor {
-    @Override
-    public void onReceive(Object o) throws Throwable {
 
+    private final static Logger log = LoggerFactory.getLogger(JustCallActor.class);
+
+    @Override
+    public void onReceive(Object message) throws Throwable {
+        if(message instanceof GameEvent){
+            GameEvent gameEvent = (GameEvent)message;
+            log.info("Received message: {}", gameEvent);
+
+            getContext().stop(getSelf());
+            getContext().system().terminate();
+        }
     }
 }
