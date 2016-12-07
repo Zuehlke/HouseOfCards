ec60d5399c30dd769e0a0d7da82dd2432398619e
lukas <lukas.hofmaier@zuehlke.com>
2016-12-07 06:52:32 +0100

    add a RouteBuilder in order to define REST endpoints "registration_info" and "match_started"

---
+++ bot/bot-server/src/main/java/com/zuehlke/hoc/examplebot/CustomRouteBuilder.java
@@ -0,0 +1,41 @@
+package com.zuehlke.hoc.examplebot;
+
+import akka.actor.ActorRef;
+import akka.camel.internal.component.CamelPath;
+import org.apache.camel.builder.RouteBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Lukas Hofmaier
+ */
+public class CustomRouteBuilder extends RouteBuilder{
+
+    private final static Logger log = LoggerFactory.getLogger(CustomRouteBuilder.class);
+
+    private String httpReceiverUri;
+    private String matchstartedActor;
+    private ActorRef httpReceiverActorRef;
+
+    public CustomRouteBuilder(ActorRef httpReceiver, ActorRef matchStartedReceiverProps){
+        this.httpReceiverUri = CamelPath.toUri(httpReceiver);
+        this.httpReceiverUri = CamelPath.toUri(matchStartedReceiverProps);
+        this.httpReceiverActorRef = httpReceiver;
+    }
+
+    public void configure() throws Exception{
+        //from("jetty:http://localhost:8081/register_info").to(this.httpReceiverUri);
+
+        rest("/register_info").post().to("direct:registerinfo");
+        rest("/match_started").post().to("direct:matchstarted");
+        rest("/round_started").post().to("direct:roundstarted");
+        rest("/yourturn").post().to("direct:yourturn");
+
+        restConfiguration().component("jetty").port(8081);
+
+        from("direct:registerinfo").process(new RegisterInfoProcessor(this.httpReceiverActorRef)).transform().constant("aye, captain");
+        from("direct:matchstarted").process(new MatchStartedProcessor(this.httpReceiverActorRef)).transform().constant("");
+
+    }
+
+}
