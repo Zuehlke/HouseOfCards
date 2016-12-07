0418b1b32edcce49ab4f756489bf7de2b63d67c0
lukas <lukas.hofmaier@zuehlke.com>
2016-12-07 06:34:07 +0100

    create RoundStartedMessage.

---
+++ game-engine/game-server-rest/src/main/java/com/zuehlke/hoc/rest/RoundStartedMessage.java
@@ -0,0 +1,37 @@
+package com.zuehlke.hoc.rest;
+
+import java.util.List;
+
+/**
+ * @author Lukas Hofmaier
+ */
+public class RoundStartedMessage {
+
+    private int round_number;
+    private List<PlayerDTO> round_players;
+    private PlayerDTO round_dealier;
+
+    public int getRound_number() {
+        return round_number;
+    }
+
+    public void setRound_number(int round_number) {
+        this.round_number = round_number;
+    }
+
+    public List<PlayerDTO> getRound_players() {
+        return round_players;
+    }
+
+    public void setRound_players(List<PlayerDTO> round_players) {
+        this.round_players = round_players;
+    }
+
+    public PlayerDTO getRound_dealier() {
+        return round_dealier;
+    }
+
+    public void setRound_dealier(PlayerDTO round_dealier) {
+        this.round_dealier = round_dealier;
+    }
+}
