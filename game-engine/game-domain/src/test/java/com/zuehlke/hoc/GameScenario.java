7fb6ec7f8a666baff28edd2cec7505d0851d37cf
lukas <lukas.hofmaier@zuehlke.com>
2016-12-07 07:58:33 +0100

    create GameScenario:testMatchStarted to grasp the order of methods called when the game starts.

--- game-engine/game-domain/src/test/java/com/zuehlke/hoc/GameScenario.java
+++ game-engine/game-domain/src/test/java/com/zuehlke/hoc/GameScenario.java
@@ -3,8 +3,11 @@ package com.zuehlke.hoc;
 
 import com.zuehlke.hoc.model.Player;
 import com.zuehlke.hoc.notification.api.PlayerNotifier;
+import com.zuehlke.hoc.notification.api.StartInfo;
 import org.junit.Test;
 import org.mockito.Mockito;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 /**
@@ -12,6 +15,8 @@ import org.mockito.Mockito;
  * shows the interaction between players and the game domain.
  */
 public class GameScenario {
+
+    private static Logger log = LoggerFactory.getLogger("PlayerNotifierLogger");
 
     @Test
     public void demo(){
@@ -53,5 +58,58 @@ public class GameScenario {
         game.playerSet(tobi, 20);   // call
 
         // ...
+    }
+
+    @Test
+    public void testMatchStarted() {
+        PlayerNotifier playerNotifier = new PlayerNotifier() {
+            @Override
+            public void sendCardInfo(String player, int card) {
+                log.info("called sendCardInfo");
+            }
+
+            @Override
+            public void playersTurn(String player, long minimumChipsForCall) {
+                log.info("called playersTurn");
+            }
+
+            @Override
+            public void broadcastGameStarts(StartInfo info) {
+                log.info("called broadcastGameStarts");
+            }
+
+            @Override
+            public void broadcastPlayerRaised(String playerName, long amount) {
+                log.info("called broadcastPlayerRaised");
+            }
+
+            @Override
+            public void broadcastPlayerCalled(String playerName) {
+                log.info("called broadcastPlayerCalled");
+            }
+
+            @Override
+            public void broadcastPlayerFolded(String playerName) {
+                log.info("called broadcastPlayerFolded");
+            }
+
+            @Override
+            public void broadcastNextRound() {
+                log.info("called broadcastNext Round");
+            }
+
+            @Override
+            public void broadcastNextMatch() {
+                log.info("called broadcastNextMatch");
+            }
+        };
+
+
+        NokerGame game = new NokerGame(3, playerNotifier);
+
+        Player tobi = game.createPlayer("tobi");
+        Player riki = game.createPlayer("riki");
+        Player miki = game.createPlayer("miki");
+
     }
 }
