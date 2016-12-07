1e817caefb1c99540cfe94e14fc68f2ab4ac6d49
lukas <lukas.hofmaier@zuehlke.com>
2016-12-07 06:32:33 +0100

    create MatchStartedMessage.

--- bot/bot-server/sendHttpPostRequestWithBody.ps1
+++ bot/bot-server/sendHttpPostRequestWithBody.ps1
@@ -1,6 +1,9 @@
 ﻿#In order to smoke test the example noker bot this script sends a HTTP post request with a JSON body to the define URI. 
-$url = "http://localhost:8081"
-$body = "{`"registrationResult`": `"CONFIRMATION`"}"
+$registerinfourl = "http://localhost:8081/register_info"
+$matchstartedurl = "http://localhost:8081/match_started"
+$body = "{`"infoMessage`": `"CONFIRMATION`", `"playerName`": `"hansdampf`", `"token`":`"8081`"}"
 $contentType = "application/json"            
 
-Invoke-RestMethod -Method Post -Uri $url -Body $body -ContentType $contentType
+Invoke-RestMethod -Method Post -Uri $registerinfourl -Body $body -ContentType $contentType
+
+Invoke-RestMethod -Method Post -Uri $matchstartedurl -Body $body -ContentType $contentType
---
+++ game-engine/game-server-rest/src/main/java/com/zuehlke/hoc/rest/MatchStartedMessage.java
@@ -0,0 +1,41 @@
+package com.zuehlke.hoc.rest;
+
+import java.util.List;
+
+/**
+ * Used to signal the start of a match to the bots.
+ *
+ * @author Lukas Hofmaier
+ */
+public class MatchStartedMessage {
+
+    private List<PlayerDTO> match_players;
+    private PlayerDTO dealer;
+    private int your_money;
+
+    public List<PlayerDTO> getMatch_players() {
+        return match_players;
+    }
+
+    public void setMatch_players(List<PlayerDTO> match_players) {
+        this.match_players = match_players;
+    }
+
+    public PlayerDTO getDealer() {
+        return dealer;
+    }
+
+    public void setDealer(PlayerDTO dealer) {
+        this.dealer = dealer;
+    }
+
+
+    public int getYour_money() {
+        return your_money;
+    }
+
+    public void setYour_money(int your_money) {
+        this.your_money = your_money;
+    }
+
+}
