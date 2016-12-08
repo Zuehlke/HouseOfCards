02c9d8054a108f5364e626bdc87abbd81f493502
ynnckth <yannickstreit@hotmail.com>
2016-12-08 13:35:08 +0100

    add notification messages according to the game protocol

--- game-engine/game-domain/src/main/java/com/zuehlke/hoc/notification/api/PlayerNotifier.java
+++ game-engine/game-domain/src/main/java/com/zuehlke/hoc/notification/api/PlayerNotifier.java
@@ -1,20 +1,26 @@
 package com.zuehlke.hoc.notification.api;
 
+
+import java.util.List;
+
 import com.zuehlke.hoc.NokerGame;
 import com.zuehlke.hoc.model.Player;
 
-import java.util.List;
 
 public interface PlayerNotifier {
 
     void sendCardInfo(String player, int card);
 
-    void playersTurn(String player, long minimumChipsForCall, NokerGame nokerGame);
-
-    void matchStarted(List<Player> player, Player dealer);
-    void broadcastPlayerRaised(String playerName, long amount);
-    void broadcastPlayerCalled(String playerName);
-    void broadcastPlayerFolded(String playerName);
-    void broadcastNextRound();
+    void broadcastGameStarted(StartInfo info);
+    void broadcastMatchStarted(List<Player> player, Player dealer);
     void broadcastNextMatch();
+    void broadcastNextRound();
+
+    void playersTurn(String player, long minimumChipsForCall, NokerGame nokerGame);
+    void broadcastMatchFinished(List<Player> matchWinners, long pot);
+    void broadcastShowdown(List<Player> players);
+    void broadcastGameFinished(Player player);
+
+    void broadcastPlayerFolded(Player player);
+    void broadcastPlayerSet(Player player, long amount);
 }
