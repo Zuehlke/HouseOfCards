9335ac3878b10e9bd9d005e7f9ab673b6c422fe0
lukas <lukas.hofmaier@zuehlke.com>
2016-12-08 11:28:12 +0100

    Define parameter of matchStarted.

--- game-engine/game-domain/src/main/java/com/zuehlke/hoc/notification/api/PlayerNotifier.java
+++ game-engine/game-domain/src/main/java/com/zuehlke/hoc/notification/api/PlayerNotifier.java
@@ -1,11 +1,15 @@
 package com.zuehlke.hoc.notification.api;
 
+import com.zuehlke.hoc.model.Player;
+
+import java.util.List;
+
 public interface PlayerNotifier {
 
     void sendCardInfo(String player, int card);
     void playersTurn(String player, long minimumChipsForCall);
 
-    void matchStarted(StartInfo info);
+    void matchStarted(List<Player> player, Player dealer);
     void broadcastPlayerRaised(String playerName, long amount);
     void broadcastPlayerCalled(String playerName);
     void broadcastPlayerFolded(String playerName);
