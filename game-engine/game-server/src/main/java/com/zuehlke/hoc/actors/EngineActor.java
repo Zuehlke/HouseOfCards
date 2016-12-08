173ac8f451f461d4c6c95535bb0d956946299c06
lukas <lukas.hofmaier@zuehlke.com>
2016-12-08 09:04:05 +0100

    implement EngineActor:setBet

--- game-engine/game-server/src/main/java/com/zuehlke/hoc/actors/EngineActor.java
+++ game-engine/game-server/src/main/java/com/zuehlke/hoc/actors/EngineActor.java
@@ -3,6 +3,9 @@ package com.zuehlke.hoc.actors;
 import com.zuehlke.hoc.NokerGame;
 import com.zuehlke.hoc.WebViewAndBotNotifier;
 import com.zuehlke.hoc.rest.RegisterMessage;
+import com.zuehlke.hoc.rest.SetMessage;
+
+import java.util.Optional;
 
 
 public class EngineActor implements IEngineActor {
@@ -25,5 +28,17 @@ public class EngineActor implements IEngineActor {
         if (this.botNotifier.registerBot(registerMessage)) {
             game.createPlayer(registerMessage.getPlayerName());
         }
+    }
+
+    @Override
+    public void setBet(SetMessage setMessage) {
+        Optional<String> playerNameOptional = this.botNotifier.getPlayer(setMessage.getUuid());
+
+        playerNameOptional.flatMap(x1 -> {
+            return this.game.getPlayer(x1);
+        }).map(x -> {
+            game.playerSet(x, setMessage.getAmount());
+            return x;
+        });
     }
 }
--- game-engine/game-server/src/main/java/com/zuehlke/hoc/actors/IEngineActor.java
+++ game-engine/game-server/src/main/java/com/zuehlke/hoc/actors/IEngineActor.java
@@ -1,9 +1,12 @@
 package com.zuehlke.hoc.actors;
 
 import com.zuehlke.hoc.rest.RegisterMessage;
+import com.zuehlke.hoc.rest.SetMessage;
 
 public interface IEngineActor {
 
     void registerPlayer(RegisterMessage registerMessage);
+
+    void setBet(SetMessage setMessage);
 
 }
